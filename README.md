44dee55d962f18fbbb789ddf3a2880d29f85026a
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-01-20 01:33:21 +0100

    initial

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # fixed-size-matrix-rs
+
 Basic arithmetic for compile-time-sized matrices using Rust const generics
---
+++ src/lib.rs
@@ -0,0 +1,14 @@
+//! Basic matrix arithmetic using const generics
+
+pub mod matrix;
+pub mod matrix_mul;
+pub mod matrix_mul_assign;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
