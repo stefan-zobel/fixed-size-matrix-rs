dedfeb827aeb7be91013ff1f368145a3b26874b7
stefan-zobel <stefan-zobel@users.noreply.github.com>
2022-01-21 18:15:18 +0100

    more idiomatic nested for loop (follow clippy advice)

--- src/scalar_mul_inplace.rs
+++ src/scalar_mul_inplace.rs
@@ -8,9 +8,9 @@ fn mul_assign_scalar<T: Numeric<T>, const ROWS: usize, const COLS: usize>(
     a: &mut [[T; COLS]; ROWS],
     scalar: T,
 ) {
-    for row in 0..ROWS {
-        for col in 0..COLS {
-            a[row][col] *= scalar;
+    for row in a.iter_mut().take(ROWS) {
+        for cell in row.iter_mut().take(COLS) {
+            *cell *= scalar;
         }
     }
 }
@@ -62,21 +62,29 @@ mod scalar_mul_inplace_tests {
         a[1][1] = 4.0f32;
         a *= 3.0f32;
         println!("SCALAR MulAssign a f32: {:?}", a);
+        assert_eq!(a[0][0], 6.0f32);
+        assert_eq!(a[1][1], 12.0f32);
         let mut a = MF::<f64, 2, 2>::new_stack();
         a[0][0] = 2.0f64;
         a[1][1] = 4.0f64;
         a *= 3.0f64;
         println!("SCALAR MulAssign a f64: {:?}", a);
+        assert_eq!(a[0][0], 6.0f64);
+        assert_eq!(a[1][1], 12.0f64);
         let mut a = MF::<i8, 2, 2>::new_stack();
         a[0][0] = 2i8;
         a[1][1] = 4i8;
         a *= 3i8;
         println!("SCALAR MulAssign a i8: {:?}", a);
+        assert_eq!(a[0][0], 6i8);
+        assert_eq!(a[1][1], 12i8);
         let mut a = MF::<i128, 2, 2>::new_stack();
         a[0][0] = 2i128;
         a[1][1] = 4i128;
         a *= 3i128;
         println!("SCALAR MulAssign a i128: {:?}", a);
+        assert_eq!(a[0][0], 6i128);
+        assert_eq!(a[1][1], 12i128);
     }
 
     #[test]
@@ -86,21 +94,29 @@ mod scalar_mul_inplace_tests {
         a[1][1] = 4.0f32;
         a *= 3.0f32;
         println!("SCALAR MulAssign a f32: {:?}", a);
+        assert_eq!(a[0][0], 6.0f32);
+        assert_eq!(a[1][1], 12.0f32);
         let mut a = MF::<f64, 2, 2>::new_heap();
         a[0][0] = 2.0f64;
         a[1][1] = 4.0f64;
         a *= 3.0f64;
         println!("SCALAR MulAssign a f64: {:?}", a);
+        assert_eq!(a[0][0], 6.0f64);
+        assert_eq!(a[1][1], 12.0f64);
         let mut a = MF::<i8, 2, 2>::new_heap();
         a[0][0] = 2i8;
         a[1][1] = 4i8;
         a *= 3i8;
         println!("SCALAR MulAssign a i8: {:?}", a);
+        assert_eq!(a[0][0], 6i8);
+        assert_eq!(a[1][1], 12i8);
         let mut a = MF::<i128, 2, 2>::new_heap();
         a[0][0] = 2i128;
         a[1][1] = 4i128;
         a *= 3i128;
         println!("SCALAR MulAssign a i128: {:?}", a);
+        assert_eq!(a[0][0], 6i128);
+        assert_eq!(a[1][1], 12i128);
     }
 
     #[test]
@@ -110,21 +126,29 @@ mod scalar_mul_inplace_tests {
         a[1][1] = 4.0f32;
         a *= 3.0f32;
         println!("SCALAR MulAssign a f32: {:?}", a);
+        assert_eq!(a[0][0], 6.0f32);
+        assert_eq!(a[1][1], 12.0f32);
         let mut a = &mut MF::<f64, 2, 2>::new_stack();
         a[0][0] = 2.0f64;
         a[1][1] = 4.0f64;
         a *= 3.0f64;
         println!("SCALAR MulAssign a f64: {:?}", a);
+        assert_eq!(a[0][0], 6.0f64);
+        assert_eq!(a[1][1], 12.0f64);
         let mut a = &mut MF::<i8, 2, 2>::new_stack();
         a[0][0] = 2i8;
         a[1][1] = 4i8;
         a *= 3i8;
         println!("SCALAR MulAssign a i8: {:?}", a);
+        assert_eq!(a[0][0], 6i8);
+        assert_eq!(a[1][1], 12i8);
         let mut a = &mut MF::<i128, 2, 2>::new_stack();
         a[0][0] = 2i128;
         a[1][1] = 4i128;
         a *= 3i128;
         println!("SCALAR MulAssign a i128: {:?}", a);
+        assert_eq!(a[0][0], 6i128);
+        assert_eq!(a[1][1], 12i128);
     }
 
     #[test]
@@ -134,20 +158,28 @@ mod scalar_mul_inplace_tests {
         a[1][1] = 4.0f32;
         a *= 3.0f32;
         println!("SCALAR MulAssign a f32: {:?}", a);
+        assert_eq!(a[0][0], 6.0f32);
+        assert_eq!(a[1][1], 12.0f32);
         let mut a = &mut MF::<f64, 2, 2>::new_heap();
         a[0][0] = 2.0f64;
         a[1][1] = 4.0f64;
         a *= 3.0f64;
         println!("SCALAR MulAssign a f64: {:?}", a);
+        assert_eq!(a[0][0], 6.0f64);
+        assert_eq!(a[1][1], 12.0f64);
         let mut a = &mut MF::<i8, 2, 2>::new_heap();
         a[0][0] = 2i8;
         a[1][1] = 4i8;
         a *= 3i8;
         println!("SCALAR MulAssign a i8: {:?}", a);
+        assert_eq!(a[0][0], 6i8);
+        assert_eq!(a[1][1], 12i8);
         let mut a = &mut MF::<i128, 2, 2>::new_heap();
         a[0][0] = 2i128;
         a[1][1] = 4i128;
         a *= 3i128;
         println!("SCALAR MulAssign a i128: {:?}", a);
+        assert_eq!(a[0][0], 6i128);
+        assert_eq!(a[1][1], 12i128);
     }
 }
